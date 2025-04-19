e295f05c9a11356332087263b0f862461a70d727
huangxl <413612464@qq.com>
2025-04-19 15:05:07 +0800

    feat: 登录注册页相互跳转

--- app/src/main/java/com/hxl/androidsr/LoginActivity.java
+++ app/src/main/java/com/hxl/androidsr/LoginActivity.java
@@ -1,14 +1,28 @@
 package com.hxl.androidsr;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // 去注册账号
+        findViewById(R.id.tv_register).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.tv_register) {
+            Intent intent = new Intent();
+            intent.setClass(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/hxl/androidsr/RegisterActivity.java
+++ app/src/main/java/com/hxl/androidsr/RegisterActivity.java
@@ -1,12 +1,13 @@
 package com.hxl.androidsr;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.hxl.androidsr.utils.LoginStatusManager;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +15,14 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         LoginStatusManager.saveLoginStatus(this, true);
+
+        findViewById(R.id.iv_back).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.iv_back) {
+            finish();
+        }
     }
 }
